b0815ca6b547fa227101a298984a065bd6d8c620
razzzzzr <alex.goryashkiev@gmail.com>
2023-03-08 14:49:00 +0300

    added base functionality of shopping cart

--- online_store/shopping_cart/urls.py
+++ online_store/shopping_cart/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'shopping_cart'
 
 urlpatterns = [
-    path('', views.example_view)
+    path('cart/', views.show_cart, name='show_cart'),
+    path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
 ]
--- online_store/shopping_cart/views.py
+++ online_store/shopping_cart/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from .models import Cart, CartItem
+from catalog.models import Product
 
 
-def example_view(request):
-    return render(request, 'shopping_cart/example.html')
+def show_cart(request):
+    cart = Cart.objects.get(user=request.user)
+    return render(request, 'shopping_cart/cart.html', {'cart': cart})
+
+
+@login_required()
+def add_to_cart(request, product_id):
+    cart, _ = Cart.objects.get_or_create(user=request.user)
+    product = get_object_or_404(Product, pk=product_id)
+    cart_item, _ = CartItem.objects.get_or_create(cart=cart, product=product)
+    cart_item.quantity += 1
+    cart_item.save()
+    return redirect('shopping_cart:show_cart')
+
+
