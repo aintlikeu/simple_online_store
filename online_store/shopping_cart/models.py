4934a0e1d9c20581680031427a0226fcd274ca0d
razzzzzr <alex.goryashkiev@gmail.com>
2023-03-09 00:29:45 +0300

    added functionality for shopping cart

--- online_store/shopping_cart/models.py
+++ online_store/shopping_cart/models.py
@@ -7,9 +7,14 @@ class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     updated_at = models.DateTimeField(auto_now_add=True)
 
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=0)
 
+    def get_cost(self):
+        return self.product.price * self.quantity
--- online_store/shopping_cart/views.py
+++ online_store/shopping_cart/views.py
@@ -18,5 +18,3 @@ def add_to_cart(request, product_id):
     cart_item.quantity += 1
     cart_item.save()
     return redirect('shopping_cart:show_cart')
-
-
