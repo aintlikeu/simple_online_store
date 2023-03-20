c46b3eeea185c7a7ae73dd44f7e0206737c4d9a7
razzzzzr <alex.goryashkiev@gmail.com>
2023-03-20 19:25:16 +0300

    "Product" model: replaced the "image" field with "image_url" field.

--- online_store/catalog/models.py
+++ online_store/catalog/models.py
@@ -9,20 +9,13 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    # CATEGORY_CHOICES = [
-    #     ('TS', 'T-shirts'),
-    #     ('SW', 'Sweatshirts'),
-    #     ('PN', 'Pants'),
-    #     ('JC', 'Jackets'),
-    # ]
 
     name = models.CharField(max_length=150)
-    # category = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default=None)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     article_number = models.CharField(max_length=50, unique=True)
     price = models.FloatField()
     stock = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='images/catalog')
+    image_url = models.URLField()
 
     def __str__(self):
         return f'{self.name} | stock: {self.stock}'
