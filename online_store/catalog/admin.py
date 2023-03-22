8cc354be674a0699523294eff2e29ddd6e00e836
razzzzzr <alex.goryashkiev@gmail.com>
2023-03-22 18:31:21 +0300

    implemented safe delete in Category & Product models (using django-safemodel library)

--- online_store/catalog/admin.py
+++ online_store/catalog/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
+from safedelete.admin import SafeDeleteAdmin, SafeDeleteAdminFilter, highlight_deleted
 from catalog.models import Product, Category
 
-# Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+
+class CustomAdmin(SafeDeleteAdmin):
+    list_display = (highlight_deleted, "highlight_deleted_field",) + SafeDeleteAdmin.list_display
+    list_filter = (SafeDeleteAdminFilter,) + SafeDeleteAdmin.list_filter
+
+    field_to_highlight = "id"
+
+
+CustomAdmin.highlight_deleted_field.short_description = CustomAdmin.field_to_highlight
+
+admin.site.register(Product, CustomAdmin)
+admin.site.register(Category, CustomAdmin)
--- online_store/catalog/models.py
+++ online_store/catalog/models.py
@@ -1,15 +1,18 @@
 from django.db import models
+from safedelete.models import SafeDeleteModel
+from safedelete.models import SOFT_DELETE_CASCADE
 
 
-class Category(models.Model):
+class Category(SafeDeleteModel):
+    _safedelete_policy = SOFT_DELETE_CASCADE
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
 
-class Product(models.Model):
-
+class Product(SafeDeleteModel):
+    _safedelete_policy = SOFT_DELETE_CASCADE
     name = models.CharField(max_length=150)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     article_number = models.CharField(max_length=50, unique=True)
